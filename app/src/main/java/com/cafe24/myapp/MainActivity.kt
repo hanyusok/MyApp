f38e1867b736cd4c600da2d5bb48cbf977aa6d81
hanyusok <hanyusok@gmail.com>
2024-11-13 11:55:21 +0900

    Update MainActivity.kt
    context rebuilt

--- app/src/main/java/com/cafe24/myapp/MainActivity.kt
+++ app/src/main/java/com/cafe24/myapp/MainActivity.kt
@@ -1,85 +1,92 @@
-package com.cafe24.myapp
+package com.cafe24
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import com.cafe24.myapp.ui.theme.MyAppTheme
+
+import androidx.compose.ui.unit.dp
+import com.cafe24.ui.theme.SupabaseAppTheme
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.postgrest.from
-import io.github.jan.supabase.postgrest.postgrest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import kotlinx.datetime.DateTimeUnit
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import java.sql.Timestamp
-import androidx.compose.material3.Surface as Surface1
-
 
 val supabase = createSupabaseClient(
-    supabaseUrl = "http://martclinic.cafe24.com:8000/rest/v1/",
+    supabaseUrl = "http://martclinic.cafe24.com:8000",
     supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzMwOTkxNjAwLAogICJleHAiOiAxODg4NzU4MDAwCn0.lA6CORXNZ8FLfK3_Y0dVo7XgavbtrdOfNZh1ursbjQQ"
 ) {
-//    install(Auth)
     install(Postgrest)
-    //install other modules
 }
+
+@Serializable
+data class Clinic (
+    @SerialName("id")
+    val id: Int,
+    @SerialName("created_at")
+    val createdAt: String,
+    @SerialName("province")
+    val province: String,
+    @SerialName("city")
+    val city: String,
+    @SerialName("clinic_name")
+    val clinicName: String,
+    @SerialName("phone_number")
+    val phoneNumber: String,
+    @SerialName("category")
+    val category: String,
+    @SerialName("rat_available")
+    val ratAvailable: Boolean,
+    @SerialName("address")
+    val address: String,
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContent {
-            MyAppTheme {
-                Surface1 (
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                    ){
+            SupabaseAppTheme {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {
                     ClinicsList()
                 }
+
             }
         }
     }
 }
 
-@Serializable
-data class Clinic (
-    val id: Int,
-    val createdAt: String,
-    val province: String,
-    val city: String,
-    val clinicName: String,
-    val phoneNumber: String,
-    val category: String,
-    val ratAvailable: Boolean,
-    val address: String,
-)
-
 @Composable
 fun ClinicsList(){
-    val clinics = remember { mutableStateListOf<Clinic>() }
+    var clinics by remember {
+        mutableStateOf<List<Clinic>>(listOf())
+    }
     LaunchedEffect(Unit) {
         withContext(Dispatchers.IO){
-            val response = supabase.from("clinics").select().decodeList<Clinic>()
-//            val response = supabase.postgrest.from("clinics").select().decodeList<Clinic>()
-            clinics.addAll(response)
+            clinics = supabase.from("clinics").select().decodeList<Clinic>()
         }
     }
-    LazyColumn{
-        items(clinics){clinic ->
-            ListItem(headlineContent = { Text(text = clinic.clinicName) })
+    LazyColumn {
+        items(clinics, key = { clinic -> clinic.clinicName}){
+            clinic -> Text(clinic.clinicName, modifier = Modifier.padding(8.dp))
         }
     }
 }
